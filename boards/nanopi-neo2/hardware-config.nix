e26cba4e812729b37ea6ee011eee3673d2469b9d
georgewhewell <georgerw@gmail.com>
2017-08-13 16:03:14 +0100

    nanopi neo2

--- boards/nanopi-neo2/hardware-config.nix
+++ boards/nanopi-neo2/hardware-config.nix
@@ -19,20 +19,20 @@ with lib;
         inherit pkgs;
     };
     uboot = pkgs.buildUBoot rec {
-        defconfig = "nanopi_neo2_defconfig";
-        targetPlatforms = [ "aarch64-linux" ];
-        filesToInstall = [ "u-boot.bin" ];
+      version = "2017.07";
+      src = pkgs.fetchurl {
+        url = "ftp://ftp.denx.de/pub/u-boot/u-boot-${version}.tar.bz2";
+        sha256 = "1zzywk0fgngm1mfnhkp8d0v57rs51zr1y6rp4p03i6nbibfbyx2k";
+      };
+      defconfig = "nanopi_neo2_defconfig";
+      targetPlatforms = [ "aarch64-linux" ];
+      filesToInstall = [ "u-boot.img" "spl/sunxi-spl.bin" ];
     };
     in {
      populateBootCommands = ''
-      # Add AML header to u-boot
-      echo "Wrapping u-boot: \
-        $(${meson-tools}/bin/amlbootsig \
-            -o u-boot-sig.bin \
-            ${uboot}/u-boot.bin)"
-
-      # Write bootloaders to sd image
-      dd conv=notrunc if=u-boot-nsih.bin of=$out seek=64
+      # Write bootloader to sd image
+      dd if=${uboot}/sunxi-spl.bin conv=notrunc of=$out bs=1024 seek=8
+      dd if=${uboot}/u-boot.img conv=notrunc of=$out bs=1024 seek=40
 
       # Populate ./boot with extlinux
       ${extlinux-conf-builder} -t 3 -c ${config.system.build.toplevel} -d ./boot
@@ -42,10 +42,10 @@ with lib;
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  boot.kernelPackages = linuxPackages_odroid;
+  boot.kernelPackages = pkgs.linuxPackages_sunxi;
   boot.initrd.kernelModules = [ "dwc2" "g_ether" "lz4" "lz4_compress" ];
   boot.initrd.availableKernelModules = [ ];
-  boot.kernelParams = ["earlyprintk" "console=ttySAC0,115200n8" "console=tty0" "brcmfmac.debug=30" "zswap.enabled=1" "zswap.compressor=lz4" "zswap.max_pool_percent=80" ];
+  boot.kernelParams = ["earlyprintk" "console=ttyS0,115200n8" "console=tty0" "brcmfmac.debug=30" "zswap.enabled=1" "zswap.compressor=lz4" "zswap.max_pool_percent=80" ];
   boot.consoleLogLevel = 7;
 
   nixpkgs.config = {
@@ -59,14 +59,20 @@ with lib;
         kernelDTB = true;
         kernelAutoModules = true;
         kernelPreferBuiltin = true;
-        /*kernelExtraConfig = ''
+        kernelExtraConfig = ''
            SND n
-           BCMDHD n
+
            ZPOOL y
            Z3FOLD y
            ZSWAP y
            CRYPTO_LZ4HC m
-        '';*/
+
+           INFINIBAND n
+           DRM_NOUVEAU n
+           DRM_AMDGPU n
+           DRM_RADEON n
+           IWLWIFI n
+        '';
         uboot = null;
         kernelTarget = "Image";
         gcc = {
--- pkgs/kernel/linux-sunxi.nix
+++ pkgs/kernel/linux-sunxi.nix
@@ -1,15 +1,15 @@
 { stdenv, hostPlatform, pkgs, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  version = "4.12";
-  modDirVersion = "4.12.0";
-  extraMeta.branch = "4.12";
+  version = "4.11";
+  modDirVersion = "4.11.10";
+  extraMeta.branch = "4.11";
 
   src = pkgs.fetchFromGitHub {
-    owner = "linux-sunxi";
-    repo = "linux-sunxi";
-    rev = "sunxi-next";
-    sha256 = "0891v6j2hpd1kq7vkd63ysmiwhmd6f1jjprzvq9p7952bik6njcd";
+    owner = "Icenowy";
+    repo = "linux";
+    rev = "0ed8d168d5426195268f1169169ae9aada1f92a6";
+    sha256 = "0nvzslw87qq9d95lpbgsrg0md5pv5pjhapksf1grz7ycc0f8p71c";
   };
 
   kernelPatches = pkgs.linux_4_12.kernelPatches;
