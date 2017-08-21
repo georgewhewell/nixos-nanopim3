20d5f8bf4c945f34b52b85fe6e098737204fa273
georgewhewell <georgerw@gmail.com>
2017-08-22 00:11:16 +0100

    fix opi pc2

--- boards/orangepi-pc2/hardware-config.nix
+++ boards/orangepi-pc2/hardware-config.nix
@@ -71,7 +71,7 @@ with lib;
            IWLWIFI n
         '';
         uboot = null;
-        kernelTarget = "zImage";
+        kernelTarget = "Image";
         gcc = {
           arch = "armv8-a";
         };
