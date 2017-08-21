9048492fb076c52f2d810eb861594692887917cf
georgewhewell <georgerw@gmail.com>
2017-08-21 21:39:18 +0100

    revert uboot upgrade

--- pkgs/boot/uboot-nanopi-m3.nix
+++ pkgs/boot/uboot-nanopi-m3.nix
@@ -5,12 +5,12 @@ pkgs.buildUBoot rec {
   src = pkgs.fetchFromGitHub {
     owner = "rafaello7";
     repo = "u-boot-nanopi-m3";
-    rev = "3a2619c02f82c70cd0a35949448790c237ff4677";
-    sha256 = "1f4v45n1i495waqvsdhq9vv2jx7hbbl6cbhddw1lqrf8q0fl4979";
+    rev = "bec6d06d29da2dfe20541d88795adb5d707c2a17";
+    sha256 = "08phny3g9q8zv0782a8idc75i8cr4kx9asl636lfz2ipjfjxq52i";
   };
 
   patches = [ ../../patches/uboot-config.patch ];
-  defconfig = "nanopim3_defconfig";
+  defconfig = "s5p6818_nanopim3_defconfig";
   targetPlatforms = [ "aarch64-linux" ];
   filesToInstall = [ "u-boot.bin" ];
 
