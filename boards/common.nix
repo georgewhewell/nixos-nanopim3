f4e8e94306cf22fb8349d5c3466f10901e26102c
georgewhewell <georgerw@gmail.com>
2017-08-23 21:46:41 +0100

    upgrade vmware

--- boards/common.nix
+++ boards/common.nix
@@ -3,12 +3,19 @@
 {
   environment.variables.GC_INITIAL_HEAP_SIZE = "100000";
   boot.kernel.sysctl."vm.overcommit_memory" = "1";
-  
+
   nixpkgs.config.packageOverrides = super: let self = super.pkgs; in {
     super.imagemagick = super.imagemagick.overrideAttrs (
       old: { arch = "aarch64"; });
     super.gcc = super.gcc6;
-
+    xorg = super.xorg // {
+      xf86videovmware = self.lib.overrideDerivation super.xorg.xf86videovmware (drv: {
+        src = pkgs.fetchurl {
+          url = "https://www.x.org/archive//individual/driver/xf86-video-vmware-13.2.1.tar.gz";
+          sha256 = "1hdb2vka7p6f6jv4kkq38a37i801nba3x0jqrnn52dklw9k8d43i";
+        };
+      });
+    };
     openssl_1_1_0 = super.openssl_1_1_0.overrideAttrs (
       old: { configureFlags = old.configureFlags ++ ["no-afalgeng"]; });
 
