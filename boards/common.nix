49886077713aacc957410783f195fcfebc0b8d11
georgewhewell <georgerw@gmail.com>
2017-08-23 23:44:47 +0100

    try to fix xf86videovmware

--- boards/common.nix
+++ boards/common.nix
@@ -13,12 +13,14 @@
     super.gcc = super.gcc6;
     xorg = super.xorg // {
       xf86videovmware = self.lib.overrideDerivation super.xorg.xf86videovmware (drv: {
+        hardeningDisable = [ "all" ];
         src = pkgs.fetchurl {
           url = "https://www.x.org/archive//individual/driver/xf86-video-vmware-13.2.1.tar.gz";
           sha256 = "1hdb2vka7p6f6jv4kkq38a37i801nba3x0jqrnn52dklw9k8d43i";
         };
       });
     };
+
     openssl_1_1_0 = super.openssl_1_1_0.overrideAttrs (
       old: { configureFlags = old.configureFlags ++ ["no-afalgeng"]; });
 
