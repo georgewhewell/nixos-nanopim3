351eafa9009f867806a14a8936aa864af1352808
georgewhewell <georgerw@gmail.com>
2017-08-25 13:59:23 +0100

    tegra gpu

--- pkgs/kernel/linux-testing.nix
+++ pkgs/kernel/linux-testing.nix
@@ -1,4 +1,4 @@
-{ stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
+{ stdenv, linux_4_12, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
   version = "4.13-rc6";
@@ -9,6 +9,13 @@ import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
     sha256 = "06jdbgihdjmq84d7km43f3w6d3hwmbnv3sgzip3nhx77l14cp7f2";
   };
+
+  kernelPatches = linux_4_12.kernelPatches ++ (if stdenv.system == "armv7l-linux" then [
+  {
+    name = "fix_tegra_gpu_build";
+    patch = ../../patches/tegra-gpu.patch;
+  }
+  ] else []);
 
   features.iwlwifi = true;
   features.efiBootStub = true;
