2c1a40ba117a028841cf2c30ee4cccec2919ab0e
georgewhewell <georgerw@gmail.com>
2017-08-26 16:24:42 +0100

    allow root emptyt pw

--- profiles/base.nix
+++ profiles/base.nix
@@ -14,10 +14,14 @@
   powerManagement.enable = lib.mkDefault true;
   hardware.bluetooth.enable = lib.mkDefault true;
   hardware.enableAllFirmware = lib.mkDefault true;
+
   networking.networkmanager.enable = lib.mkDefault (pkgs.stdenv.system == "aarch64-linux");
 
   # dont need :)
   services.nixosManual.enable = lib.mkDefault false;
   programs.man.enable = lib.mkDefault false;
   programs.info.enable = lib.mkDefault false;
+
+  # Allow the user to log in as root without a password.
+  users.extraUsers.root.initialHashedPassword = "";
 }
