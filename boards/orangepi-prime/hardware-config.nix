b310c45f2bbeabb49b745b52a387d6a3358f12dc
georgewhewell <george.whewell@aig.com>
2017-08-29 11:17:15 +0100

    patches

--- boards/orangepi-prime/hardware-config.nix
+++ boards/orangepi-prime/hardware-config.nix
@@ -26,6 +26,16 @@ with lib;
         rev = "2d7cb5b426e7e0cdf684d7f8029ad132d7a8d383";
         sha256 = "18dvbmapijq59gaz418pz939r3vwaz6a29m5jlxfacywiggjgyrw";
       };
+      patches = [
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/add-missing-gpio-compatibles.patch"
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/add-nanopineoplus2.patch"
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/add-pinebook-defconfig.patch"
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/disable-usb-keyboards.patch"
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/enable-DT-overlays-support.patch"
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/fix-sopine-defconfig.patch"
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/pll1-clock-fix-h5.patch"
+        "${pkgs.armbian}/patch/u-boot/u-boot-sun50i-dev/sunxi-boot-splash.patch"
+      ];
       nativeBuildInputs = with pkgs;
         [ gcc6 bc dtc swig1 which python2 ];
       preBuild = "cp ${pkgs.bl31-a64} bl31.bin";
--- pkgs/boot/bl31-a64.nix
+++ pkgs/boot/bl31-a64.nix
@@ -11,23 +11,6 @@ pkgs.stdenv.mkDerivation rec {
       sha256 = "1hnr1123ik840ihvnck8agqm9r79x8hbaq4d4bvn1q9ayd2d5sy0";
     };
 
-    patches = [
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/01-add-SMC-mailbox.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/02-add-handler-boilerplate.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/03-add-clock-handling.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/04-implement-CPU-clock.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/05-allow-setting-CPU-voltage.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/06-refactor-poweroff.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/07-refactor-power-code.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/08-add-DVFS.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/09-add-THS-sensor.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/10-add-device-power-state.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/add-SRAM-mapping-for-SCPI.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/enable-a53-errata-workaround.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw1/set-rsb-to-nonsec.patch"
-      "${pkgs.armbian}/patch/atf/atf-sun50iw2/add-pc2-shutdown.patch"
-    ];
-
     dontStrip = true;
     hardeningDisable = [ "all" ];
     nativeBuildInputs = [ pkgs.gcc6 ];
