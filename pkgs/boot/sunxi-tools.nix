7c884a229ef99e639ad31507519a81c00bc8663d
georgewhewell <george.whewell@aig.com>
2017-08-31 16:42:13 +0100

    fix sunxi-tools sha256

--- pkgs/boot/sunxi-tools.nix
+++ pkgs/boot/sunxi-tools.nix
@@ -8,7 +8,7 @@ pkgs.stdenv.mkDerivation rec {
       owner = "linux-sunxi";
       repo = "sunxi-tools";
       rev = "d9b1d7e7dff1e70fea91a3f259e9e8ac9508de35";
-      sha256 = "1bvshfa9pa012yzdwapi3nalpgcwmfq7d3n3w3mlr357a6kq64qk";
+      sha256 = "073s7psjqgvpv8lybjfqicmbwlq9kkznkyivsd0m25v099la5fy0";
     };
 
     hardeningDisable = [ "all" ];
