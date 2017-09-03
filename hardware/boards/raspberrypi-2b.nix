6bbd0e76474429bff0738eda3bd9bd5a70a00200
georgewhewell <georgerw@gmail.com>
2017-09-03 01:33:22 +0100

    overclocks for rpi

--- hardware/boards/raspberrypi-2b.nix
+++ hardware/boards/raspberrypi-2b.nix
@@ -3,6 +3,12 @@
 with lib;
 let
   platforms = (import ../platforms.nix);
+  overclocksettings = ''
+    arm_freq=1100
+    core_freq=250
+    sdram_freq=500
+    over_voltage=4
+  '';
 in
 {
   imports = [
@@ -13,6 +19,7 @@ in
     (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf $NIX_BUILD_TOP/boot/)
     cp ${pkgs.ubootRaspberryPi}/u-boot.bin boot/u-boot-rpi.bin
     echo 'kernel u-boot-rpi.bin' > boot/config.txt
+    echo "${overclocksettings}" >> boot/config.txt
   '';
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
