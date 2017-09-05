24dc4dd8c1693ac3057fed75187e6b79c9c56715
georgewhewell <georgerw@gmail.com>
2017-09-05 19:16:23 +0100

    remove rpi overclocking

--- hardware/boards/raspberrypi-2b.nix
+++ hardware/boards/raspberrypi-2b.nix
@@ -19,7 +19,6 @@ in
     (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf $NIX_BUILD_TOP/boot/)
     cp ${pkgs.ubootRaspberryPi}/u-boot.bin boot/u-boot-rpi.bin
     echo 'kernel u-boot-rpi.bin' > boot/config.txt
-    echo "${overclocksettings}" >> boot/config.txt
   '';
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
