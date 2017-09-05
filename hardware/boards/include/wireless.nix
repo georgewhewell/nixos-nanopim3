2f7f706583e28804065c70208ccfc321a3144597
georgewhewell <georgerw@gmail.com>
2017-09-05 22:14:22 +0100

    disable wireless manager

--- hardware/boards/include/wireless.nix
+++ hardware/boards/include/wireless.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-    /*networking.wireless.enable = true;*/
     networking.networkmanager.enable = false;
     networking.networkmanager-small.enable = lib.mkDefault true;
 }
