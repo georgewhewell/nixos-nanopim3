768aee28f52afe7f40f6849c0454643d8e935547
georgewhewell <georgerw@gmail.com>
2017-09-05 19:21:42 +0100

    update libffi to maybe fix pythonPackages.cffi

--- pkgs/overrides.nix
+++ pkgs/overrides.nix
@@ -8,4 +8,21 @@
   llvm = pkgs.llvm.overrideAttrs (
     old: { doCheck = false; });
 
+  libffi = pkgs.libffi.overrideDerivation (
+    old: rec {
+      src = pkgs.fetchFromGitHub {
+        owner = "libffi";
+        repo = "libffi";
+        rev = "b23091069adce469dc38fbcc9fd8ac9085d3c9d7"; # master @ 4/09/17
+        sha256 = "0h9rpdh3vw2idw1i487jr5qh10i3s7gpsbn8izb0whknnrz4v98q";
+      };
+      nativeBuildInputs = [ pkgs.autoreconfHook pkgs.texinfo ];
+      postFixup = ''
+        mkdir -p "$dev/"
+        substituteInPlace "$dev/lib/pkgconfig/libffi.pc" \
+          --replace 'includedir=''${libdir}/libffi-3.2.1' "includedir=$dev"
+      '';
+    }
+  );
+
 }
