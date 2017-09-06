f94a3e5103f40068e9dee7f56ad840a4f1b50601
georgewhewell <georgerw@gmail.com>
2017-09-06 21:57:19 +0100

    another laborious attempt at overriding cffi

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,14 +1,14 @@
 { self, super }:
 
-let
-  pythonOverrides = (selfpkgs: selfpkgs // {
-    cffi = super.cffi.overrideAttrs (oldAttrs: {
-      doCheck = false; doInstallCheck = false; checkPhase = "";
-    });
-  });
-in {
-  pythonPackages = pythonOverrides super.pythonPackages;
-  python2Packages = pythonOverrides super.python2Packages;
+rec {
+  python = super.python.override {
+     packageOverrides = python-self: python-super: {
+       cffi = super.python2Packages.cffi.overrideAttrs (oldAttrs: {
+         doCheck = false; doInstallCheck = false; checkPhase = "";
+       });
+     };
+  };
+  pythonPackages = python.pkgs;
 
   gnome3 = super.gnome3 // {
     networkmanager_iodine = super.gnome3.networkmanager_iodine.overrideAttrs (
