7fc048d9730649c04ee2a2713b379d41094aee7a
georgewhewell <george.whewell@aig.com>
2017-09-13 11:26:52 +0100

    odroid c2 kernel -> 4.13.0

--- pkgs/kernel/linux-amlogic.nix
+++ pkgs/kernel/linux-amlogic.nix
@@ -1,13 +1,13 @@
 { stdenv, hostPlatform, pkgs, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  version = "4.13-rc6";
-  modDirVersion = "4.13.0-rc6";
+  version = "4.13";
+  modDirVersion = "4.13.0";
   extraMeta.branch = "4.13";
 
   src = pkgs.fetchurl {
     url = "https://git.kernel.org/pub/scm/linux/kernel/git/khilman/linux-amlogic.git/snapshot/linux-amlogic-${version}.tar.gz";
-    sha256 = "0f632p9gahb12dp78b4g5mxsyan1lyzscysxalp5g5824nrrybyq";
+    sha256 = "1x8fww17ga2rm1q56plzsknkccmj3ily1bqp2q3rx984kk4bc5k7";
   };
   kernelPatches = pkgs.linux_4_12.kernelPatches;
 
