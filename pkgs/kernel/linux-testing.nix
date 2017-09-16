eaddf734c85ba760b6006aee1dd9d68afaa7f954
georgewhewell <georgerw@gmail.com>
2017-09-16 10:31:10 +0100

    update 4.13

--- pkgs/kernel/linux-testing.nix
+++ pkgs/kernel/linux-testing.nix
@@ -1,16 +1,15 @@
-{ stdenv, linux_4_12, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
+{ stdenv, linux_4_13, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  version = "4.13-rc6";
-  modDirVersion = "4.13.0-rc6";
+  version = "4.13.1";
   extraMeta.branch = "4.13";
 
   src = fetchurl {
-    url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "06jdbgihdjmq84d7km43f3w6d3hwmbnv3sgzip3nhx77l14cp7f2";
+    url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
+    sha256 = "1kp1lsf4314af7crpqkd2x1zx407a97r7rz3zhhskbilvsifgkny";
   };
 
-  kernelPatches = linux_4_12.kernelPatches ++ (if stdenv.system == "armv7l-linux" then [
+  kernelPatches = linux_4_13.kernelPatches ++ (if stdenv.system == "armv7l-linux" then [
     {
       name = "fix_tegra_gpu_build";
       patch = ../../patches/tegra-gpu.patch;
