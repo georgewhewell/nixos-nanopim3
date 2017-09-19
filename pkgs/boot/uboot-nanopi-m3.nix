31efc26f5715e7f6d7bbb17a8a35f7e57c79b5c4
georgewhewell <georgerw@gmail.com>
2017-09-19 23:41:52 +0100

    nanopi-m3: uboot

--- pkgs/boot/uboot-nanopi-m3.nix
+++ pkgs/boot/uboot-nanopi-m3.nix
@@ -5,10 +5,11 @@ pkgs.buildUBoot rec {
   src = pkgs.fetchFromGitHub {
     owner = "rafaello7";
     repo = "u-boot-nanopi-m3";
-    rev = "0bf4d63cd56e9d8f842397186a67851b3bc3befc";
-    sha256 = "1cip2znk1zngzf42lsmfdzi0hgwjxwkc8cn48wzrxvqr4iky8q1j";
+    rev = "4431a3b75afb86fad09d0ae42d1dddb82aa11b5e";
+    sha256 = "04kdb78lmf1n1pjrpx7vsw07hiy1w65fa2lbxgg9kgxzjz2898jw";
   };
 
+  patches = [ ../../patches/nanopi-m3-uboot-config.patch ];
   installFlags = [ "SPL" ];
   defconfig = "nanopim3_defconfig";
   targetPlatforms = [ "aarch64-linux" ];
