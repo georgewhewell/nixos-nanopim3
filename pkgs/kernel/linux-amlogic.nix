752406d64be60f282d07ab0a3e9ab7a023ee8bba
georgewhewell <georgerw@gmail.com>
2017-09-21 19:30:00 +0100

    update amlogic kernel

--- pkgs/kernel/linux-amlogic.nix
+++ pkgs/kernel/linux-amlogic.nix
@@ -1,15 +1,16 @@
 { stdenv, hostPlatform, pkgs, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  version = "4.13-rc6";
-  modDirVersion = "4.13.0-rc6";
-  extraMeta.branch = "4.13";
+  version = "4.14-rc1";
+  modDirVersion = "4.14.0-rc1";
+  extraMeta.branch = "4.14";
 
-  src = pkgs.fetchurl {
-    url = "https://git.kernel.org/pub/scm/linux/kernel/git/khilman/linux-amlogic.git/snapshot/linux-amlogic-${version}.tar.gz";
-    sha256 = "0f632p9gahb12dp78b4g5mxsyan1lyzscysxalp5g5824nrrybyq";
+  src = pkgs.fetchgit {
+    url = "https://kernel.googlesource.com/pub/scm/linux/kernel/git/khilman/linux-amlogic.git";
+    rev = "b42a362e6d10c342004b183defcb9940331b6737";
+    sha256 = "1x4760fdr8x6nmlm46qs669i1fah56zdsgpdfpkmbxnfnwzrl15d";
   };
-  kernelPatches = pkgs.linux_4_12.kernelPatches;
+  kernelPatches = pkgs.linux_4_13.kernelPatches;
 
   features.iwlwifi = false;
   features.efiBootStub = true;
