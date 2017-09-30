b4d483d7e337ee6ff67cddf3dc9b3f143fce764f
georgewhewell <georgerw@gmail.com>
2017-09-30 21:29:18 +0100

    use friendlyarm bsp

--- hardware/platforms.nix
+++ hardware/platforms.nix
@@ -77,6 +77,7 @@ let
 in {
   inherit aarch64-multiplatform;
   inherit armv7l-hf-multiplatform;
+  armv7l-sunxi = armv7l-hf-multiplatform // { kernelBaseConfig = "sunxi_defconfig"; kernelTarget = "zImage"; };
   aarch64-sunxi = aarch64-multiplatform // { kernelBaseConfig = "sunxi_defconfig"; kernelTarget = "zImage"; };
   aarch64-nanopi-m3 = aarch64-multiplatform // { kernelBaseConfig = "nanopim3_defconfig"; };
 }
