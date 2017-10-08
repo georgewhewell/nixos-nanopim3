8ae95ad3d0bc47d9b802a9fcf6e6e9460024bf0f
georgewhewell <georgerw@gmail.com>
2017-10-08 19:12:59 +0100

    fix release-pkgs

--- release-pkgs.nix
+++ release-pkgs.nix
@@ -1,7 +1,12 @@
 { supportedSystems ? [ "x86_64-linux" "aarch64-linux" "armv7l-linux" ] }:
 
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (self: super: import pkgs/overlay.nix { inherit self super; })
+      (self: super: import pkgs/top-level.nix { pkgs = self; })
+    ];
+  };
   hardware = import ./hardware { inherit pkgs; };
   forAllSystems = pkgs.lib.genAttrs supportedSystems;
   buildPackages = pkg: forAllSystems (system: pkgs.lib.hydraJob (pkg system));
