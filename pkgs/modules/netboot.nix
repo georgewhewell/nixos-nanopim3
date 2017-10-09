c8edb0e04d7276c87cdd5785122f83a16b25caeb
georgewhewell <georgerw@gmail.com>
2017-10-10 00:28:11 +0100

    hack

--- pkgs/modules/netboot.nix
+++ pkgs/modules/netboot.nix
@@ -27,7 +27,7 @@ with lib;
     boot.loader.grub.enable = false;
 
     # !!! Hack - attributes expected by other modules.
-    system.boot.loader.kernelFile = "zImage";
+    system.boot.loader.kernelFile = pkgs.stdenv.platform.kernelTarget;
     boot.initrd.network.enable = true;
 
     environment.systemPackages = [ ];
