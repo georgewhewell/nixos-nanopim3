9527a61ec3e2c6635d6a4e3fcea3c71f223d71c2
georgewhewell <georgerw@gmail.com>
2017-10-17 20:24:10 +0100

    hc1 bootloader

--- hardware/boards/odroid-hc1.nix
+++ hardware/boards/odroid-hc1.nix
@@ -12,7 +12,11 @@ in
 
   system.build.sd = {
     installBootloader = ''
-      ${pkgs.odroid-xu3-bootloader}/bin/sd_fuse-xu3 $out
+      dd if=${pkgs.odroid-xu3-bootloader}/bl1.bin.hardkernel of=$out seek=1 conv=notrunc
+      dd if=${pkgs.odroid-xu3-bootloader}/bl2.bin.hardkernel.720k_uboot of=$out seek=31 conv=notrunc
+      dd if=${pkgs.odroid-xu3-bootloader}/u-boot-dtb.bin of=$out bs=512 seek=63 conv=notrunc
+      dd if=${pkgs.odroid-xu3-bootloader}/tzsw.bin.hardkernel of=$out seek=1503 conv=notrunc
+      dd if=/dev/zero of=$out seek=2015 bs=512 count=32 conv=notrunc
     '';
   };
 
