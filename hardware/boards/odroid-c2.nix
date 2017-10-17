f7f197580bbf3f3d3276e2be9a738f0492d6a166
georgewhewell <georgerw@gmail.com>
2017-10-17 19:05:11 +0000

    odroid c2

--- hardware/boards/odroid-c2.nix
+++ hardware/boards/odroid-c2.nix
@@ -9,30 +9,32 @@ in
      ./include/common.nix
   ];
 
-  nixpkgs.config.writeBootloader = ''
-    # Ref: http://git.denx.de/?p=u-boot.git;a=blob_plain;f=board/amlogic/odroid-c2/README;hb=HEAD
-    export HKDIR=${pkgs.fip_create.src}
+  system.build.sd = {
+    installBootloader = ''
+      # Ref: http://git.denx.de/?p=u-boot.git;a=blob_plain;f=board/amlogic/odroid-c2/README;hb=HEAD
+      export HKDIR=${pkgs.fip_create.src}
 
-    echo "Creating FIP"
-    ${pkgs.fip_create}/bin/fip_create \
-      --bl30  $HKDIR/fip/gxb/bl30.bin \
-      --bl301 $HKDIR/fip/gxb/bl301.bin \
-      --bl31  $HKDIR/fip/gxb/bl31.bin \
-      --bl33  ${pkgs.uboot-odroid-c2}/u-boot.bin \
-      --dump \
-      fip.bin
+      echo "Creating FIP"
+      ${pkgs.fip_create}/bin/fip_create \
+        --bl30  $HKDIR/fip/gxb/bl30.bin \
+        --bl301 $HKDIR/fip/gxb/bl301.bin \
+        --bl31  $HKDIR/fip/gxb/bl31.bin \
+        --bl33  ${pkgs.uboot-odroid-c2}/u-boot.bin \
+        --dump \
+        fip.bin
 
-    echo "Inserting bl2"
-    cat $HKDIR/fip/gxb/bl2.package fip.bin > boot_new.bin
+      echo "Inserting bl2"
+      cat $HKDIR/fip/gxb/bl2.package fip.bin > boot_new.bin
 
-    echo "Wrapping u-boot"
-    ${pkgs.meson-tools}/bin/amlbootsig boot_new.bin u-boot.img
+      echo "Wrapping u-boot"
+      ${pkgs.meson-tools}/bin/amlbootsig boot_new.bin u-boot.img
 
-    # Write bootloaders to sd image
-    dd if=${pkgs.bl1-odroid-c2.default} of=$out conv=notrunc bs=1 count=442
-    dd if=${pkgs.bl1-odroid-c2.default} of=$out conv=notrunc bs=512 skip=1 seek=1
-    dd if=u-boot.img of=$out conv=notrunc bs=512 skip=96 seek=97
-  '';
+      # Write bootloaders to sd image
+      dd if=${pkgs.bl1-odroid-c2.default} of=$out conv=notrunc bs=1 count=442
+      dd if=${pkgs.bl1-odroid-c2.default} of=$out conv=notrunc bs=512 skip=1 seek=1
+      dd if=u-boot.img of=$out conv=notrunc bs=512 skip=96 seek=97
+    '';
+  };
 
   boot.kernelPackages = pkgs.linuxPackages_4_12;
   boot.extraTTYs = [ "ttyAML0" ];
