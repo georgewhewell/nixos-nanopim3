e9c0008de314daa26994e0e4b7ebf991862918bf
georgewhewell <georgerw@gmail.com>
2017-10-17 20:38:03 +0100

    use upstream bl

--- hardware/boards/odroid-hc1.nix
+++ hardware/boards/odroid-hc1.nix
@@ -12,11 +12,7 @@ in
 
   system.build.sd = {
     installBootloader = ''
-      dd if=${pkgs.odroid-xu3-bootloader}/bl1.bin.hardkernel of=$out seek=1 conv=notrunc
-      dd if=${pkgs.odroid-xu3-bootloader}/bl2.bin.hardkernel.720k_uboot of=$out seek=31 conv=notrunc
-      dd if=${pkgs.odroid-xu3-bootloader}/u-boot-dtb.bin of=$out bs=512 seek=63 conv=notrunc
-      dd if=${pkgs.odroid-xu3-bootloader}/tzsw.bin.hardkernel of=$out seek=1503 conv=notrunc
-      dd if=/dev/zero of=$out seek=2015 bs=512 count=32 conv=notrunc
+      ${pkgs.ubootOdroidXU3}/bin/sd_fuse-xu3 $out
     '';
   };
 
