1dbe24d68ca2822f75af352241e180db353e5a75
georgewhewell <georgerw@gmail.com>
2017-10-30 19:43:09 +0000

    update sunxi64 kernel

--- pkgs/kernel/linux-sunxi64.nix
+++ pkgs/kernel/linux-sunxi64.nix
@@ -1,5 +1,7 @@
 { stdenv, hostPlatform, pkgs, perl, buildLinux, ... } @ args:
 
+with pkgs;
+
 let
   armbianPatches = map (path: {
     name = "armbian-${path}";
@@ -8,7 +10,6 @@ let
     "sunxi64-dev/a64-DT-DVFS.patch"
     "sunxi64-dev/a64-DVFS-cpu-supply.patch"
     "sunxi64-dev/a64-pll_cpux-test.patch"
-    "sunxi64-dev/add-h3-h5-THS.patch"
     "sunxi64-dev/add-overlay-compilation-support.patch"
     "sunxi64-dev/add-spi-flash-pc2.patch"
     "sunxi64-dev/add-sun50i-ths.patch"
@@ -16,25 +17,24 @@ let
     "sunxi64-dev/add-sy8106a-driver.patch"
     "sunxi64-dev/add-ths-DT-h5.patch"
     "sunxi64-dev/arm64_increasing_DMA_block_memory_allocation_to_2048.patch"
-    "sunxi64-dev/cpufreq-dt-platdev-Automatically-create-cpufreq-device-with-OPP-v2.patch"
     "sunxi64-dev/enable-fsl-timer-errata.patch"
     "sunxi64-dev/fix-i2c2-reg-property.patch"
     "sunxi64-dev/scripts-dtc-import-updates.patch"
     "sunxi64-dev/spidev-remove-warnings.patch"
  ];
 in import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  version = "4.13.y";
-  modDirVersion = "4.13.2";
-  extraMeta.branch = "4.13";
+  version = "4.14-rc6";
+  modDirVersion = "4.14.0-rc6";
+  extraMeta.branch = "4.14";
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "Icenowy";
     repo = "linux";
-    rev = "sunxi64-${version}";
-    sha256 = "1l69aadh71gv3rww6ypjm9rgahhv93y04jxfvvsbpliys677m36n";
+    rev = "8b92245f06bfe048e99902336208ee5a15f63c2e";
+    sha256 = "0y0d54sl1rvvzb5fcdl67p1ccpcp64rkr5vgzq1ryhazn1z7iadm";
   };
 
-  kernelPatches = pkgs.linux_4_13.kernelPatches ++ armbianPatches;
+  kernelPatches = linux_4_13.kernelPatches ++ armbianPatches;
 
   features.iwlwifi = false;
   features.efiBootStub = true;
