325c27d77d286eaf0731d568b5a396c9038d8baf
georgewhewell <georgerw@gmail.com>
2017-10-31 09:20:44 +0000

    hc1 boot

--- hardware/boards/odroid-hc1.nix
+++ hardware/boards/odroid-hc1.nix
@@ -12,10 +12,10 @@ in
 
   system.build.sd = {
     installBootloader = ''
-      dd if=${pkgs.odroid-xu3-bootloader}/bl1.bin.hardkernel of=$out seek=1 conv=notrunc
-      dd if=${pkgs.odroid-xu3-bootloader}/bl2.bin.hardkernel.720k_uboot of=$out seek=31 conv=notrunc
-      dd if=${pkgs.odroid-xu3-bootloader}/u-boot-dtb.bin of=$out bs=512 seek=63 conv=notrunc
-      dd if=${pkgs.odroid-xu3-bootloader}/tzsw.bin.hardkernel of=$out seek=1503 conv=notrunc
+      dd if=${pkgs.uboot-odroid-xu4}/bl1.bin.hardkernel of=$out seek=1 conv=notrunc
+      dd if=${pkgs.uboot-odroid-xu4}/bl2.bin.hardkernel.720k_uboot of=$out seek=31 conv=notrunc
+      dd if=${pkgs.uboot-odroid-xu4}/u-boot-dtb.bin of=$out bs=512 seek=63 conv=notrunc
+      dd if=${pkgs.uboot-odroid-xu4}/tzsw.bin.hardkernel of=$out seek=1503 conv=notrunc
       dd if=/dev/zero of=$out seek=2015 bs=512 count=32 conv=notrunc
     '';
   };
@@ -23,7 +23,7 @@ in
   boot.extraTTYs = [ "ttySAC2" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   nixpkgs.config.platform = platforms.armv7l-hf-base;
-  system.build.bootloader = pkgs.odroid-xu3-bootloader;
+  system.build.bootloader = pkgs.uboot-odroid-xu4;
 
   networking.hostName = "odroid-hc1";
 
